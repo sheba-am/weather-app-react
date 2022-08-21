71e919bcc4b55c808122cc0d69700174595f1c2f
sheba-am <saba.ameri1378@gmail.com>
2022-08-21 22:19:42 +0430

    add geo api search

---
+++ weather-app-react/src/components/search/Search.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import { AsyncPaginate } from "react-select-async-paginate"
+import { GEO_API_URL, geoApiOptions } from '../../api';
+function Search({ onSearchChange }) {
+    const [search, setSearch] = useState(null)
+
+    const loadOptions = (inputValue) => {
+        return fetch(`${GEO_API_URL}/cities?minPopulation=100000&namePrefix=${inputValue}`, geoApiOptions)
+            .then(response => response.json())
+            .then(response => {
+                return {
+                    options: response.data.map((city) => {
+                        return {
+                            value: `${city.latitude} ${city.longitude}`,
+                            label: `${city.name}, ${city.countryCode}`,
+                        }
+                    })
+                }
+            })
+            .catch(err => console.error(err));
+    }
+    const handleOnChange = (searchData) => {
+        setSearch(searchData);
+        onSearchChange(searchData)
+    }
+    return (
+        <AsyncPaginate
+            placeholder="Search for ciry"
+            debounceTimeout={600}  // number of requests
+            value={search}
+            onChange={handleOnChange}
+            loadOptions={loadOptions}
+        />
+
+    )
+}
+
+export default Search
