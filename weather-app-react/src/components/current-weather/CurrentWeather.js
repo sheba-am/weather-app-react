0410b6f9ea8da93804dab333b3ac07bb9ebc4785
sheba-am <saba.ameri1378@gmail.com>
2022-08-23 16:56:48 +0430

    add static css

--- weather-app-react/src/components/current-weather/CurrentWeather.js
+++ weather-app-react/src/components/current-weather/CurrentWeather.js
@@ -3,7 +3,39 @@ import './CurrentWeather.css'
 
 function CurrentWeather() {
   return (
-    <div>CurrentWeather</div>
+    <div className='weather'>
+      <div className='top'>      
+        <div>
+          <p className='city'>belgrade</p>
+          <p className='weather-description'>sunny</p>
+        </div>
+        <img alt='weather' className='weather-icon' src='icons/01d.png'></img>
+      </div>
+      <div className='bottom'>
+        <p className='temperature'> 18°C</p>
+        <div className='details'>
+          <div className='parameter-row' >
+            <span className='parameter-label'>Details</span>
+          </div>
+          <div className='paramter-row'>
+            <span className='parameter-label'>feels like</span>
+            <span className='parameter-value'>22°C</span>
+          </div>
+          <div className='paramter-row'>
+            <span className='parameter-label'>Wind</span>
+            <span className='parameter-value'>2 m/s</span>
+          </div>          
+          <div className='paramter-row'>
+            <span className='parameter-label'>Humidity</span>
+            <span className='parameter-value'>15%</span>
+          </div>
+          <div className='paramter-row'>
+            <span className='parameter-label'>Pressure</span>
+            <span className='parameter-value'>25 hPa</span>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
