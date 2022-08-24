452af4ce8e303e349df171d3463e18d8ea1a5690
sheba-am <saba.ameri1378@gmail.com>
2022-08-24 17:26:03 +0430

    make current weather dynamic

--- weather-app-react/src/components/current-weather/CurrentWeather.js
+++ weather-app-react/src/components/current-weather/CurrentWeather.js
@@ -1,37 +1,37 @@
 import React from 'react'
 import './CurrentWeather.css'
 
-function CurrentWeather() {
+function CurrentWeather({data}) {
   return (
     <div className='weather'>
       <div className='top'>      
         <div>
-          <p className='city'>belgrade</p>
-          <p className='weather-description'>sunny</p>
+          <p className='city'>{data.city}</p>
+          <p className='weather-description'>{data.weather[0].description}</p>
         </div>
-        <img alt='weather' className='weather-icon' src='icons/01d.png'></img>
+        <img alt='weather' className='weather-icon' src={`icons/${data.weather[0].icon}.png`} ></img>
       </div>
       <div className='bottom'>
-        <p className='temperature'> 18°C</p>
+        <p className='temperature'> {Math.round(data.main.temp)}°C</p>
         <div className='details'>
           <div className='parameter-row' >
             <span className='parameter-label'>Details</span>
           </div>
           <div className='paramter-row'>
-            <span className='parameter-label'>feels like</span>
-            <span className='parameter-value'>22°C</span>
+            <span className='parameter-label'> Feels like</span>
+            <span className='parameter-value'>{Math.round(data.main.feels_like)}°C</span>
           </div>
           <div className='paramter-row'>
             <span className='parameter-label'>Wind</span>
-            <span className='parameter-value'>2 m/s</span>
+            <span className='parameter-value'>{Math.round(data.wind.speed)} m/s</span>
           </div>          
           <div className='paramter-row'>
             <span className='parameter-label'>Humidity</span>
-            <span className='parameter-value'>15%</span>
+            <span className='parameter-value'>{Math.round(data.main.humidity)}%</span>
           </div>
           <div className='paramter-row'>
             <span className='parameter-label'>Pressure</span>
-            <span className='parameter-value'>25 hPa</span>
+            <span className='parameter-value'>{Math.round(data.main.pressure)} hPa</span>
           </div>
         </div>
       </div>
